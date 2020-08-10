65b9d06087d566019da326eaa676c4dd059e9aec
Gonzalo Jusid <gonzalo.jusid@despegar.com>
2020-08-09 23:35:22 -0300

    Finished tests

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,22 +1,22 @@
-import { searchMatrixWord, letterExists } from '../../src/utils/utils';
+import { searchMatrixWord, letterExists } from './utils';
 
 describe('>>> Utils', () => {
-    const matrix = [
+    const matrix: Array<Array<string>> = [
         ['A', 'L', 'O'],
         ['L', 'X', 'L'],
         ['O', 'L', 'A']
     ];
 
     test('+++ Should search the word ALO on the matrix', () => {
-        const wordToFind = "ALO";
-
+        const wordToFind: string = "ALO";
+    
         expect(searchMatrixWord(matrix, wordToFind)).toBe(4);
     });
-
+    
     test('+++ Should validate if one letter exists on a position', () => {
-        const x = -1;
-        const y = -1;
-
+        const x: number = -1;
+        const y: number = -1;
+    
         expect(letterExists(matrix, x, y)).toBeFalsy();
     });
 });
